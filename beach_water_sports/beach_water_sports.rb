1d154f651e8e4dc50a18067a18c78479162f5785
Linus Oleander <linus@oleander.nu>
2011-06-06 14:32:12 +0200

    Adding comments

--- beach_water_sports/beach_water_sports.rb
+++ beach_water_sports/beach_water_sports.rb
@@ -1,21 +1,30 @@
 class BeachWaterSports
   def initialize
-    @orders = []
+    @orders  = []
+    @amounts = 12
   end
   
+  # @order Order An order object
+  # @return Integer An ordernumber
+  # Makes it possible to store order for later useage.
   def add_order(order)
     order_number = random
     @orders[order_number] = order
     order_number
   end
   
+  # @nr Integer Deletes the given order numver
+  # @return Order The order that has been deleted
+  # Delete any given order
   def delete_order(nr)
     @orders.delete_at(nr)
   end
   
+  # @return Integer An available random number
+  # Returns a random number that hasn't been used by the {@orders} array
   def random
     begin
-      r = rand(12) + 1
+      r = rand(@amounts) + 1
     end while not @orders[r].nil?
     
     r
@@ -23,14 +32,24 @@ class BeachWaterSports
 end
 
 class Order < Struct.new(:name, :type, :time)
+  # @return Integer Time in seconds since order was created
+  # Caching is turned off
   def time_since
     Time.now.to_i - self[:time].to_i
   end
   
+  # @return String The owners name
+  # Removes whitespace and newlines @ 
+  # end and start of string
+  # Caching is turned on
   def name
     @_name ||= self[:name].strip
   end
   
+  # @return String Type of order
+  # Removes whitespace and newlines @ 
+  # end and start of string
+  # Caching is turned on
   def type
     @_type ||= self[:type].strip
   end
@@ -51,15 +70,20 @@ loop do
   
   params = []
   
+  # The given params from the user must begin with a number
+  # A switch case would have been nicer, but this is good enough
   if gets =~ /^(\d+)/
-    if $1 == "1"
+    if $1 == "1" # Adding an order
       ["Your name", "What type do you want?"].each do |what|
         puts what
         params << gets
       end
+      # Save the current time
       params << Time.now
+      
+      # Adding order
       puts "Your order number is #{bws.add_order(Order.new(*params))}"
-    elsif $1 == "2"
+    elsif $1 == "2" # Removing order
       puts "Enter your order number."
       order = bws.delete_order(gets.to_i)
       
@@ -68,10 +92,11 @@ loop do
       else
         puts "Wrong order number, please try again."
       end
-    elsif $1 == "3"
+    elsif $1 == "3" # Stops main loop
       puts "Thanks for using Beach Water Sports, bye!"; break
     end
   else
-    puts "You have to enter a valid option"
+    # If the user enters any invalid option, this will be printed.
+    puts "You have to enter a valid option."
   end
 end
